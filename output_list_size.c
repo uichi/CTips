a17b07afe21a04964ee10ede74ebb83326c41fd3
Uichi <ykonishi@protonmail.com>
2018-04-01 23:58:28 +0900

    Fix the output text.

--- output_list_size.c
+++ output_list_size.c
@@ -3,9 +3,10 @@
 int main(void)
 {
   char *list[] = {"Ubuntu", "CentOS", "Arch", "fedora"};
-  printf("Entire list size is %d byte.\n", (int) sizeof list);
-  printf("First element size is %d byte.\n", (int) sizeof list[0]);
+  printf("Entire list size is %d bytes.\n", (int) sizeof list);
+  printf("First element size is %d bytes.\n",  (int) sizeof list[0]);
   // 配列の要素全体の大きさ / 配列の要素一つ分の大きさ
   int list_size = sizeof list / sizeof list[0];
-  printf("List size of element is %d byte.\n", list_size);
+  printf("Entire elements are %d.\n", list_size);
+  return 0;
 }
